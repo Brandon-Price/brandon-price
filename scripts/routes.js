cb230ffc37af12ef83d53921605e425085afeed2
Brandon-Price <brandon.samuel.price@gmail.com>
2024-08-04 23:39:33 -0500

    Add Route for Repo Button

--- scripts/routes.js
+++ scripts/routes.js
@@ -1,4 +1,15 @@
 function goToGithub(){
-    window.open("https://github.com/Brandon-Price").focus()
+    window.open("https://github.com/Brandon-Price").focus();
     return;
+}
+
+function goToRepo(url){
+    if(url == 'Ecommerce'){
+        window.open("https://github.com/Brandon-Price/SWE-Project").focus();
+        return;
+    }
+    else{
+        window.open("https://github.com/Brandon-Price").focus();
+        return;
+    }
 }
