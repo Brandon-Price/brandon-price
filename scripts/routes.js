81ff1ce015d07aaf0f5863e494fbc46719223667
Brandon-Price <brandon.samuel.price@gmail.com>
2024-08-09 00:55:14 -0500

    Make new page with updated css

--- scripts/routes.js
+++ scripts/routes.js
@@ -3,6 +3,11 @@ function goToGithub(){
     return;
 }
 
+function goToLinkedIn(){
+    window.open('https://www.linkedin.com/in/brandonprice-/').focus();
+    return;
+}
+
 function goToRepo(url){
     if(url == 'Ecommerce'){
         window.open("https://github.com/Brandon-Price/SWE-Project").focus();
